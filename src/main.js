1342de0b00489c3fd6d5c65b0def547160840ff1
Seyfullah Tıkıç <stikic@gmail.com>
2021-10-09 23:37:27 +0300

    First commit

--- src/main.js
+++ src/main.js
@@ -2,3 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
+
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
